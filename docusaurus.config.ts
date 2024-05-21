56255a247ffa3cb5878526edc999db5b2405c20a
LoveOrange <orange24k@hotmail.com>
2024-05-21 09:11:14 +0800

    add leetcode 645

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,15 +37,13 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/loveorange/coding101/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/loveorange/coding101/tree/main/",
         },
         theme: {
           customCss: "./src/css/custom.css",
@@ -128,7 +126,7 @@ const config: Config = {
             },
             {
               label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              href: "https://github.com/loveorange/coding101",
             },
           ],
         },
